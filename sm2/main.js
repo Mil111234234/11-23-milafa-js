37a79da552c9337bd236711732ad6a9c10272574
Афанасьева Милана Евгеньевна <152304@mck-ktits.ru>
2024-09-21 09:19:23 +0300

    mske sm2

---
+++ sm2/main.js
@@ -0,0 +1,14 @@
+prompt('Как тебя зовут?');
+let age = prompt('сколько тебе лет?');
+if (age < 20){
+    alert("Тебе меньше 20")
+} else if(age> 20){
+    for(var i = 0; i < 5; i++){
+        alert('Привет')
+    }
+}
+
+let vopros = confirm('Хочешь учить js?');
+if (true){
+    alert('Супер!')
+}
